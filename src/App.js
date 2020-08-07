2547c21c1f1c15e2edc8835096a9c3f0787c39c5
nicholas <nicholas.computacao@outlook.com>
2020-08-07 01:32:29 -0300

    DIO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DIO from './images/DIO.png'
+import galaxy from './images/galaxy.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <React.Fragment>
+      <div className="topDiv">
+        <svg>
+          <defs>
+            <pattern id="img1" patternUnits="userSpaceOnUse" width="100" height="100">
+              <image href={galaxy} x="0" y="0" width="100" height="100" />
+            </pattern>
+          </defs>
+          <polygon points="0,0 0,1000, 1400,0 0,0"></polygon>
+        </svg>
+      </div>
+      <div className = "title">
+        <h1>Stardust Crusaders</h1>
+      </div>
+      <div className = "about">
+        <h2>Quando acontece?</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A história acontece em 1989, começando com o Jotaro, protagonista da série,
+          na prisão.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h2>Sinopse:</h2>
+        <p>
+          Jotaro estava na prisão, por pedido dele mesmo, pois manifestou um poder sobrenatural,
+          conhecido como Stand. Após um tempo e encontro com Joseph e seu amigo, Avdol, e posteriormente
+          Kakkyoin, os quatro vão em direção ao Egito, para matar DIO e salvar a mãe de Jotaro, que estava
+          tendo complicações, por causa do stand.
+        </p>
+      </div>
+      <div className="image">
+        <img src={DIO}></img>
+      </div>
+    </React.Fragment>
   );
 }
 
